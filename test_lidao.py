2ee10b1f95dfb8633e43fc5a050a0d78bd83818e
LidaOA <lidao.alouda@gmail.com>
2024-10-03 13:11:52 +0200

    Ajout de modifications sur la branche exemple_de_branche_lidao

---
+++ test_lidao.py
@@ -0,0 +1,16 @@
+class TestLidao:
+
+    def __init__(self):
+
+        self.message = "Test ajout de fichier Lidao"
+
+    def afficher_message(self):
+
+        print(self.message)
+
+        # Instanciation de la classe et appel de la méthode
+
+        test = TestLidao() # Création de l'objet test
+
+        test.afficher_message() # Appel de la méthode pour afficher le message
+
