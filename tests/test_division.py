0b528fec54790798d18de56f70a3c5f82fc296c4
Robin Guiavarch <robin.guiavarch@gmail.com>
2024-10-09 20:01:05 +0200

    Reformatage des fichiers avec black

--- tests/test_division.py
+++ tests/test_division.py
@@ -2,20 +2,24 @@ import pytest
 from app.division import Division
 import logging
 
+
 def test_division_succes():
     """Teste une division normale."""
     division = Division(10, 2)
     assert division.diviser() == 5.0
 
+
 def test_division_zero():
     """Teste la division par zéro."""
     division = Division(10, 0)
-    assert division.diviser() == float('inf')
+    assert division.diviser() == float("inf")
+
 
 def test_division_type_error():
     """Teste si une exception est levée pour des valeurs non numériques."""
     with pytest.raises(ValueError, match="Les valeurs doivent être des nombres."):
         Division("10", 2).diviser()
+
 
 def test_logs_division():
     """Vérifie que les logs sont bien écrits lors d'une division."""
